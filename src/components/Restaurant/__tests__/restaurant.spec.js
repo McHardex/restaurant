82e8742ae7e0ce877af2532db665c84eb87812e2
OluwabukunmiAdebisi <bukunmiadebisi@gmail.com>
2019-09-30 09:34:58 +0100

    sort restaurant by minimum cost

--- src/components/Restaurant/__tests__/restaurant.spec.js
+++ src/components/Restaurant/__tests__/restaurant.spec.js
@@ -15,11 +15,11 @@ describe('<Restaurant />', () => {
     expect(wrapper.state().restaurants[0].favorite).toEqual(true);
   });
 
-  it('should set favorite key in previously updated object in restaurant array back to false', () => {
-    const e = { target: { id: 0 }};
+  it('should update second item in restaurant array to have favorite key of true', () => {
+    const e = { target: { id: 1 }};
     const wrapper = shallow(<Restaurant />);
-    wrapper.find('.fav-button').at(0).simulate('click', e);
-    expect(wrapper.state().restaurants[0].favorite).toEqual(false);
+    wrapper.find('.fav-button').at(1).simulate('click', e);
+    expect(wrapper.state().restaurants[1].favorite).toEqual(true);
   });
 
   it('should filter restaurant lists based on search parameter', () => {
@@ -29,59 +29,65 @@ describe('<Restaurant />', () => {
     expect(wrapper.state().restaurants.length).toEqual(1);
   });
 
-  it('should update first item in restaurant array to have favorite key of true', () => {
-    const e = { target: { id: 0 }};
-    const wrapper = shallow(<Restaurant />);
-    wrapper.find('.fav-button').at(0).simulate('click', e);
-    expect(wrapper.state().restaurants[0].favorite).toEqual(true);
-  });
-
-  it('should update second item in restaurant array to have favorite key of true', () => {
-    const e = { target: { id: 1 }};
-    const wrapper = shallow(<Restaurant />);
-    wrapper.find('.fav-button').at(1).simulate('click', e);
-    expect(wrapper.state().restaurants[1].favorite).toEqual(true);
-  });
-
   it('should sort restaurant list by best match', () => {
     const wrapper = shallow(<Restaurant />);
     wrapper.find('.best-match').simulate('click');
     expect(wrapper.state().restaurants[0].name).toEqual('Tanoshii Sushi');
   });
 
+  it('should set first item in restaurant array to have favorite key to false', () => {
+    const e = { target: { id: 0 }};
+    const wrapper = shallow(<Restaurant />);
+    wrapper.find('.fav-button').at(0).simulate('click', e);
+    expect(wrapper.state().restaurants[0].favorite).toEqual(false);
+  });
+
+  it('should set second item in restaurant array to have favorite key of false', () => {
+    const e = { target: { id: 1 }};
+    const wrapper = shallow(<Restaurant />);
+    wrapper.find('.fav-button').at(0).simulate('click', e);
+    expect(wrapper.state().restaurants[1].favorite).toEqual(false);
+  });
+
   it('should sort restaurant list by newest', () => {
     const wrapper = shallow(<Restaurant />);
     wrapper.find('.newest').simulate('click');
-    expect(wrapper.state().restaurants[2].name).toEqual('De Amsterdamsche Tram');
+    expect(wrapper.state().restaurants[0].name).toEqual('Tanoshii Sushi');
   });
 
   it('should sort restaurant list by ratingAverage', () => {
     const wrapper = shallow(<Restaurant />);
     wrapper.find('.ratingAverage').simulate('click');
-    expect(wrapper.state().restaurants[3].name).toEqual('Lunchpakketdienst');
+    expect(wrapper.state().restaurants[0].name).toEqual('De Amsterdamsche Tram');
   });
 
   it('should sort restaurant list by distance', () => {
     const wrapper = shallow(<Restaurant />);
     wrapper.find('.distance').simulate('click');
-    expect(wrapper.state().restaurants[2].name).toEqual('Aarti 2');
+    expect(wrapper.state().restaurants[0].name).toEqual('Tanoshii Sushi');
   });
 
   it('should sort restaurant list by popularity', () => {
     const wrapper = shallow(<Restaurant />);
     wrapper.find('.popularity').simulate('click');
-    expect(wrapper.state().restaurants[0].name).toEqual('Tanoshii Sushi');
+    expect(wrapper.state().restaurants[0].name).toEqual('Lunchpakketdienst');
   });
 
   it('should sort restaurant list by Average Product Price', () => {
     const wrapper = shallow(<Restaurant />);
     wrapper.find('.averageProductPrice').simulate('click');
-    expect(wrapper.state().restaurants[2].name).toEqual('De Amsterdamsche Tram');
+    expect(wrapper.state().restaurants.length).toEqual(19);
   });
 
   it('should sort restaurant list by delivery cost', () => {
     const wrapper = shallow(<Restaurant />);
     wrapper.find('.deliveryCosts').simulate('click');
-    expect(wrapper.state().restaurants[0].name).toEqual('Tanoshii Sushi');
+    expect(wrapper.state().restaurants.length).toEqual(19);
+  });
+
+  it('should sort restaurant list by minimum cost', () => {
+    const wrapper = shallow(<Restaurant />);
+    wrapper.find('.minCost').simulate('click');
+    expect(wrapper.state().restaurants[0].name).toEqual('De Amsterdamsche Tram');
   });
 });
